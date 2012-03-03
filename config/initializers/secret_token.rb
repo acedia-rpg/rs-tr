cf227c3f41bb9479384875b2ef9b7986f82ae22c
Gary Allison <acedia.rpg@gmail.com>
2012-03-02 21:09:42 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RocksmithTracker::Application.config.secret_token = 'f9e8add2ee2e24006000366e887c7d171dbf8c2722926477798bc3e646ca5c19c50875889982195023e1933b5784eb9ee82fc7405a1933f5ad0a503e4222e442'
